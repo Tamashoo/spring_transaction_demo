3414f8c90b477240d845e5756137da30cf490adb
Tamashoo <72484282+Tamashoo@users.noreply.github.com>
2025-06-29 21:48:48 +0900

    Update UsersController.java

--- src/main/java/com/example/demo/UsersController.java
+++ src/main/java/com/example/demo/UsersController.java
@@ -1,8 +1,8 @@
 package com.example.demo;
 
-import jakarta.transaction.Transactional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
